5bb1adcd5a3f2c616eb97c91c61e4c4dd78c726f
Nattapat-Tatiyarat <peak_ntp@hotmail.com>
2022-10-16 16:57:50 +0700

    cors fixed

--- src/api/http.js
+++ src/api/http.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const HTTP = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL || `http://localhost:3405`,
   headers: {
+    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
   },
 });
