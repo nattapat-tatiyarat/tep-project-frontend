57b78fc2247eeecd66af7f8c10000333cfb1a12f
Jook Gru <jookgru@hotmail.com>
2023-05-04 23:12:02 +0700

    Page Title

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,13 @@ export default {
   data() {
     return {};
   },
+  mounted() {
+                  document.title = "Smart Grow Room";
+                  var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = '../assets/sprout.svg';
+    document.getElementsByTagName('head')[0].appendChild(link);
+                      }
 };
 </script>
